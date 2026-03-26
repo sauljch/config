59b6f3b5f1a954cf530e924c2c70340680785a3a
Saul Cervantes <sauljch@yahoo.com>
2026-03-26 07:00:45 -0700

    chore(wezterm): Update opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,6 @@ config.font_size = 15.0
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.colors = require('cyberdream')
-config.window_background_opacity = 0.80
+config.window_background_opacity = 0.95
 
 return config
